3db1e5b1448589d725aaac12f7c62b4e5e407ba1
Chris Hanson <org/chris-hanson/cph>
2019-05-07 23:50:48 -0700

    Treat strings returned from primitives as UTF-8 coded.
    In particular, make sure that filenames are treated this way, because this is
    normal for modern unix systems.  This change mostly affects filenames, but also
    environment variables and command-line strings.
    
    This is necessary because strings passed to primitives are converted to UTF-8
    bytevectors.  Otherwise, a non-ASCII filename returned by a primitive won't be
    converted back to the same bytes when passed to another primitive.

--- src/runtime/string.scm
+++ src/runtime/string.scm
@@ -2087,6 +2087,11 @@ USA.
       string
       (string->utf8 string)))
 
+(define (string-from-primitive string)
+  (if (legacy-string? string)
+      (utf8->string (legacy-string->bytevector string))
+      string))
+
 (define-integrable (every-loop proc ref string start end)
   (let loop ((i start))
     (if (fix:< i end)
