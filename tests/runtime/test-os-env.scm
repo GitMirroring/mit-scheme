e2328051e77a5fd0f13509ef1546b2a0cca2e727
Taylor R Campbell <campbell+mit-scheme@mumble.net>
2022-05-01 11:56:56 +0000

    Test get-environment-variables.

--- tests/runtime/test-os-env.scm
+++ tests/runtime/test-os-env.scm
@@ -43,3 +43,28 @@ USA.
 	 (if old
 	     (set-environment-variable! var old)
 	     (delete-environment-variable! var)))))))
+
+(define-test 'list-smoke
+  (lambda ()
+    (expect-error
+     (lambda ()
+       (get-environment-variables)))))
+
+(define-test 'set-list-assoc
+  (lambda ()
+    (expect-error
+     (lambda ()
+       (let* ((var "FOOBAR")
+	      (val "MUMBLEFROTZ")
+	      (old (get-environment-variable var)))
+	 (dynamic-wind
+	   (lambda () 0)
+	   (lambda ()
+	     (set-environment-variable! var val)
+	     (assert-equal (assoc var (get-environment-variables))
+			   (cons var val))
+	     0)
+	   (lambda ()
+	     (if old
+		 (set-environment-variable! var old)
+		 (delete-environment-variable! var)))))))))
