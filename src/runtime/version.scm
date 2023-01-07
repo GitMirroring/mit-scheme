f169a305f29a1b018d2501a39e9c8851036e662b
Chris Hanson <org/chris-hanson/cph>
2023-01-06 22:05:17 -0800

    Update version numbers for release.

--- src/runtime/version.scm
+++ src/runtime/version.scm
@@ -43,7 +43,7 @@ USA.
 
 (add-boot-init!
  (lambda ()
-   (add-subsystem-identification! "Release" '(12 0 90))
+   (add-subsystem-identification! "Release" '(12 1))
    (run-now-and-after-restore!
     (lambda ()
       (add-subsystem-identification! "Microcode" (get-microcode-version))))
